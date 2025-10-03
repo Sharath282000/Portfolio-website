1b83fb51de3921ff213c7f3ab41aba8da87ba307
Sharath282000 <sharathmurugan28@gmail.com>
2025-10-03 17:36:38 +0530

    Content changes in Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
           </a>
         </span>
         <p className={styles.description}>
-         Building solutions as an <b>Oracle ERP Analyst at Deloitte 🌐</b>, coding creative projects as a <b>Full Stack Developer 💻</b>, and finding joy in <b>Cricket 🏏</b>
+         Building solutions as an <b>Oracle Cloud Analyst at Deloitte💼</b>, coding creative projects as a <b>Full Stack Developer💻</b>, and finding joy in <b>Cricket🏏and Cinema🎥</b>
         </p>
       </div>
     </section>
